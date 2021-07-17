5fa80d42fda2811a077e0e2a0074756519192744
ryohei19 <ryohei.basket@gmail.com>
2021-07-17 04:09:37 +0000

    finish 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '937e85917b7103e61fafd5507729dd419b6cb0e78d6584595ce0712a38e7f6e5ba47c7a7f23ff17de6cce837892caca69d50ef04aeaf1c29bea9f461a5a2f13b'
